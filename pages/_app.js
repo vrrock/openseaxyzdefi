4b5e9810c29c0620a0abd0a47205a43b50467ed0
vrrock <63670007+vrrock@users.noreply.github.com>
2022-03-19 13:59:01 -0400

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
+
+
+const supportedChainIds = [4]
+const connectors = {
+  injected: {},
+}
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Header from '../components/header'
+import Hero from '../components/Hero'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
